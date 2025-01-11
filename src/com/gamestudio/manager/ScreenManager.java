8ceff9338450045c276719e39418ff5a7358a18f
ViniciusOliveira2004 <vinicius.oliveira3@ufv.br>
2025-01-11 18:17:35 -0300

    feat: adicionando classe para gerenciar telas

---
+++ src/com/gamestudio/manager/ScreenManager.java
@@ -0,0 +1,42 @@
+package com.gamestudio.manager;
+
+
+import javax.swing.JPanel;
+import java.awt.CardLayout;
+import com.gamestudio.interfaces.StartScreen;
+import com.gamestudio.interfaces.teste;
+import com.gamestudio.state.MenuState;
+
+public class ScreenManager {
+    private CardLayout cardLayout;
+    private JPanel panels;
+    private StartScreen startScreen;
+    private String currentScreen;
+
+    public ScreenManager() {
+        cardLayout = new CardLayout();
+        panels = new JPanel(cardLayout);
+
+        startScreen = new StartScreen(new MenuState(startScreen, this));
+        panels.add(startScreen, "Menu");
+
+        currentScreen = "Menu";
+    }
+
+    public StartScreen getStartScreen() {
+        return startScreen;
+    }
+
+    public JPanel getPanels() {
+        return panels;
+    }
+
+    public void setCurrentScreen(String currentScreen) {
+        this.currentScreen = currentScreen;
+    }
+
+    public void show() {
+        cardLayout.show(panels, currentScreen);
+    }
+}
+
