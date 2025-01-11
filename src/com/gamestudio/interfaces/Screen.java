aaf3e0c416c13f5c28ef58f1d7c56d9f7f0c0c52
ViniciusOliveira2004 <vinicius.oliveira3@ufv.br>
2025-01-10 22:42:59 -0300

    feat: adicionando Loop do jogo em Screen

--- src/com/gamestudio/interfaces/Screen.java
+++ src/com/gamestudio/interfaces/Screen.java
@@ -6,11 +6,13 @@ import java.awt.CardLayout;
 import java.awt.Dimension;
 import java.awt.Toolkit;
 
-public class Screen extends JFrame {
+public class Screen extends JFrame implements Runnable{
     public static final int width = 1000;
     public static final int height = 600;
     private CardLayout cardLayout;
     private JPanel panels;
+    StartScreen startScreen;
+
 
     public Screen() {
         super("Mega man");
@@ -18,11 +20,14 @@ public class Screen extends JFrame {
         Toolkit toolkit = this.getToolkit();
         Dimension desktopSolution = toolkit.getScreenSize();
 
+        startScreen = new StartScreen();
+        
         cardLayout = new CardLayout();
         panels = new JPanel(cardLayout);
-        panels.add(new StartScreen(), "Menu");
+        panels.add(startScreen, "Menu");
 
         this.add(panels);
+        addKeyListener(startScreen);
         this.setBounds(
                 (desktopSolution.width - width) / 2,
                 (desktopSolution.height - height) / 2,
@@ -33,6 +38,7 @@ public class Screen extends JFrame {
 
     public void open() {
         super.setVisible(true);
+        new Thread(this).start();
     }
 
     public void close() {
@@ -41,6 +47,35 @@ public class Screen extends JFrame {
 
     public void show(String namePanel) {
         cardLayout.show(panels, namePanel);
+    }
+
+    @Override
+    public void run() {
+        long previousTime = System.nanoTime();
+        long currentTime;
+        long sleepTime;
+        long framePeriod = 1000000000 / 30; // 60 FPS
+
+        while (true) {
+            // Atualizar a tela atual
+            JPanel activePanel = (JPanel) panels.getComponent(0);
+            if (activePanel instanceof Runnable runnable) {
+                runnable.run();
+            }
+
+            // Controle de FPS
+            currentTime = System.nanoTime();
+            sleepTime = framePeriod - (currentTime - previousTime);
+            if (sleepTime > 0) {
+                try {
+                    Thread.sleep(sleepTime / 1000000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            previousTime = System.nanoTime();
+        }   
+
     }
 
     public static void main(String arg[]){
