b641326d1f3a5e04662b25c781e87d2e948d44b6
ViniciusOliveira2004 <vinicius.oliveira3@ufv.br>
2025-01-12 23:04:12 -0300

    refactor: deixando o projeto com apenas uma tela

--- src/com/gamestudio/interfaces/Screen.java
+++ src/com/gamestudio/interfaces/Screen.java
@@ -8,28 +8,70 @@ import javax.swing.JPanel;
 
 import com.gamestudio.state.State;
 
-public abstract class Screen extends JPanel implements Runnable, KeyListener{
+public class Screen extends JPanel implements Runnable, KeyListener{
     private State state;
+    Thread gameThread;
 
     public Screen(State state) {
         this.state = state;
     }
 
-    public State getState() {
-        return state;
+    public void setState(State state) {
+        this.state = state;
     }
-    @Override
-    public abstract void paint(Graphics g);
+
+    public void startGame(){
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
 
     @Override
-    public abstract void run();
+    public void paint(Graphics g) {
+        g.drawImage(state.getBufferedImage(), 0, 0, null);
+    }
 
     @Override
-    public abstract void keyPressed(KeyEvent e);
+    public void keyPressed(KeyEvent e) {
+        this.state.setPressedButton(e.getKeyCode());
+    }
 
     @Override
-    public abstract void keyReleased(KeyEvent e);
+    public void keyReleased(KeyEvent e) {
+
+    }
 
     @Override
-    public  abstract void keyTyped(KeyEvent e);
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void run() {
+
+        long previousTime = System.nanoTime();
+        long currentTime;
+        long sleepTime;
+
+        long period = 1000000000/80;
+
+        while(true){
+
+            state.update();
+            state.render();
+            repaint();
+
+            currentTime = System.nanoTime();
+            sleepTime = period - (currentTime - previousTime);
+            try{
+
+                    if(sleepTime > 0)
+                            Thread.sleep(sleepTime/1000000);
+                    else Thread.sleep(period/2000000);
+
+            }catch(Exception e){}
+
+            previousTime = System.nanoTime();
+        }
+
+    }
 }
