ddabc7db96d39c830d349f84f77b42a3cacc2048
ViniciusOliveira2004 <vinicius.oliveira3@ufv.br>
2025-01-15 22:08:31 -0300

    feat: adicionando ParticularObjectManager

---
+++ src/com/gamestudio/manager/ParticularObjectManager.java
@@ -0,0 +1,87 @@
+package com.gamestudio.manager;
+
+import java.awt.Graphics2D;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+import com.gamestudio.elements.ParticularObject;
+import com.gamestudio.state.GameState;
+
+public class ParticularObjectManager {
+
+    protected List<ParticularObject> particularObjects;
+
+    private GameState gameState;
+    
+    public ParticularObjectManager(GameState gameState) {
+        particularObjects = Collections.synchronizedList(new LinkedList<ParticularObject>());
+        this.gameState = gameState;
+    }
+    
+    public GameState getGameWorld(){
+        return gameState;
+    }
+    
+    public void addObject(ParticularObject particularObject){
+        
+        synchronized(particularObjects){
+            particularObjects.add(particularObject);
+        }
+        
+    }
+    
+    public void RemoveObject(ParticularObject particularObject){
+        synchronized(particularObjects){
+        
+            for(int id = 0; id < particularObjects.size(); id++){
+                
+                ParticularObject object = particularObjects.get(id);
+                if(object == particularObject)
+                    particularObjects.remove(id);
+
+            }
+        }
+    }
+    
+    public ParticularObject getCollisionWidthEnemyObject(ParticularObject object){
+        synchronized(particularObjects){
+            for(int id = 0; id < particularObjects.size(); id++){
+                
+                ParticularObject objectInList = particularObjects.get(id);
+
+                if(object.getTeamType() != objectInList.getTeamType() && 
+                        object.getBoundForCollisionWithEnemy().intersects(objectInList.getBoundForCollisionWithEnemy())){
+                    return objectInList;
+                }
+            }
+        }
+        return null;
+    }
+    
+    public void UpdateObjects(){
+        
+        synchronized(particularObjects){
+            for(int id = 0; id < particularObjects.size(); id++){
+                
+                ParticularObject object = particularObjects.get(id);
+                
+                
+                if(!object.isObjectOutOfCameraView()) object.Update();
+                
+                if(object.getCurrentState() == ParticularObject.DEATH){
+                    particularObjects.remove(id);
+                }
+            }
+        }
+        
+    }
+    
+    public void draw(Graphics2D g2){
+        synchronized(particularObjects){
+            for(ParticularObject object: particularObjects)
+                if(!object.isObjectOutOfCameraView()) object.draw(g2);
+        }
+    }
+	
+}
