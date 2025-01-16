a46a47fcea853ca31626b3a7fb81e49f8f9ea3d8
Edthedeveloper2000 <edmarcosmendes2019@gmail.com>
2025-01-16 13:54:49 -0300

    feat: add classe Projectile (Bullet)

---
+++ src/com/gamestudio/elements/Projectile.java
@@ -0,0 +1,30 @@
+package com.gamestudio.elements;
+
+import com.gamestudio.state.GameState;
+import java.awt.Graphics2D;
+
+/**
+ * TODO separar robot em robot e GameObject. Robot será superclass de SmartRobot e DumbRobot,
+ * enquanto projectile herda de GameObject (ou outro nome mais descritivo)
+ */
+public abstract class Projectile extends Robot {
+
+    public Projectile(int x, int y, int width, int height, int mass, int damage, GameState gameState) {
+        super(x, y, width, height, mass, 1, gameState);
+        setDamage(damage);
+    }
+
+    public abstract void draw(Graphics2D g2d);
+
+    public void update(){
+        super.update();
+        setPosX(getPosX() + getSpeedX());
+        setPosY(getPosY() + getSpeedY());
+        Robot object = getGameState().robotManager.getCollisionWidthEnemyObject(this);
+        if(object!=null && object.getCurrentState() == ALIVE){
+            object.beHurt(getDamage());
+            System.out.println("Bullet set behurt for enemy");
+        }
+    }
+
+}
