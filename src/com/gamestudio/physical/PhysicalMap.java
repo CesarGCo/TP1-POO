8bed36ace943eeb7635fc3655655532c83b60aeb
ViniciusOliveira2004 <vinicius.oliveira3@ufv.br>
2025-01-17 18:23:30 -0300

    refactor: sincronizando física do mapa com câmera

--- src/com/gamestudio/physical/PhysicalMap.java
+++ src/com/gamestudio/physical/PhysicalMap.java
@@ -5,6 +5,7 @@ import java.awt.Graphics;
 import java.awt.Rectangle;
 
 import com.gamestudio.state.GameState;
+import com.gamestudio.elements.Camera;
 import com.gamestudio.elements.GameElement;
 import com.gamestudio.manager.DataLoader;
 
@@ -146,13 +147,13 @@ public class PhysicalMap extends GameElement {
     
     public void draw(Graphics g2){
         
-        //Camera camera = getGameWorld().camera;
+        Camera camera = getGameState().camera;
         
         g2.setColor(Color.GRAY);
         for(int i = 0;i< phys_map.length;i++)
             for(int j = 0;j<phys_map[0].length;j++)
-                if(phys_map[i][j]!=0) g2.fillRect((int) getPosX() + j*tileSize, 
-                        (int) getPosY() + i*tileSize, tileSize, tileSize);
+                if(phys_map[i][j]!=0) g2.fillRect((int) (getPosX() - camera.getPosX() + j*tileSize), 
+                        (int) (getPosY() - camera.getPosY() + i*tileSize), tileSize, tileSize);
         
     }
     
