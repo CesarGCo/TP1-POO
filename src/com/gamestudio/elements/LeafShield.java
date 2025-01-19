e9462f01f7c43f003f46c647ca5a72b4ffdc71ae
Edthedeveloper2000 <edmarcosmendes2019@gmail.com>
2025-01-19 10:19:52 -0300

    feat: adiciona classe LeafShield (ataque do woodman)

---
+++ src/com/gamestudio/elements/LeafShield.java
@@ -0,0 +1,36 @@
+package com.gamestudio.elements;
+
+import com.gamestudio.effect.Animation;
+import com.gamestudio.manager.DataLoader;
+import com.gamestudio.state.GameState;
+
+import java.awt.*;
+
+public class LeafShield extends Projectile {
+    private final Animation forwardAnimation;
+    private final Animation backAnimation;
+
+    public LeafShield(float x, float y, GameState gameState) {
+        super(x, y, 40, 40, 1.0f, 8, gameState);
+
+        forwardAnimation = DataLoader.getInstance().getAnimation("wood_man_leaf_shield");
+        backAnimation = DataLoader.getInstance().getAnimation("wood_man_leaf_shield");
+        backAnimation.flipAllImage();
+    }
+
+    @Override
+    public Rectangle getBoundForCollisionWithEnemy() {
+        return getBoundForCollisionWithMap();
+    }
+
+    @Override
+    public void draw(Graphics2D g2) {
+        if(this.getDirection() == LEFT) {
+            forwardAnimation.Update(System.nanoTime());
+            forwardAnimation.draw((int) (getPosX() - getGameState().camera.getPosX()), (int) (getPosY() - getGameState().camera.getPosY()), g2);
+        } else {
+            backAnimation.Update(System.nanoTime());
+            backAnimation.draw((int) (getPosX() - getGameState().camera.getPosX()), (int) (getPosY() - getGameState().camera.getPosY()), g2);
+        }
+    }
+}
