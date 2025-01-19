0a9239be13b848ef305767fc5d6ccc421d9a41a3
Edthedeveloper2000 <edmarcosmendes2019@gmail.com>
2025-01-19 13:33:12 -0300

    feat: atualiza vida do wood-man

--- src/com/gamestudio/elements/WoodMan.java
+++ src/com/gamestudio/elements/WoodMan.java
@@ -24,7 +24,7 @@ public class WoodMan extends SmartRobot {
     private final Projectile leafShield = new LeafShield(getPosX(), getPosY(), getGameState());
 
     public WoodMan(int x, int y, GameState gameState) {
-        super(x, y, 34, 31, 0.1f, 2, gameState);
+        super(x, y, 34, 31, 0.1f, 32, gameState);
         setCurrentAction(INTRO);
         stateStartTime = System.currentTimeMillis();
         this.setDirection(WoodMan.LEFT);
