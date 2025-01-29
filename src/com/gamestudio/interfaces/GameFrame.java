b7a9f4c2dbf54a9de8d5e0846582e5bcdd3c8384
ViniciusOliveira2004 <vinicius.oliveira3@ufv.br>
2025-01-29 17:15:54 -0300

    style: comentando o código das interfaces

--- src/com/gamestudio/interfaces/GameFrame.java
+++ src/com/gamestudio/interfaces/GameFrame.java
@@ -8,28 +8,35 @@ import java.awt.Dimension;
 import com.gamestudio.manager.DataLoader;
 import com.gamestudio.manager.StateManager;
 
+
+// A classe GameFrame é responsável por representar a janela do Jogo
+// Ele representa uma janela com barra de título, bordas e botões padrão
 public class GameFrame extends JFrame {
     public static final int width = 1000;
     public static final int height = 600;
     private final StateManager stateManager;
 
-
     public GameFrame() {
+        //Adicionando títula à janela:
         super("Mega man");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         Toolkit toolkit = this.getToolkit();
         Dimension desktopSolution = toolkit.getScreenSize();
+
+        // Adicionando todos os Assets do projeto:
         try {
             DataLoader.getInstance().LoadData();
         } catch (IOException e) {
             e.printStackTrace();
         }
+
         this.stateManager = new StateManager();
 
+        //Adicionando ouvinte de eventos de teclado:
         addKeyListener(stateManager.getScreen());
         add(stateManager.getScreen());
         
-        
+        // Setando dimensões e posição da janela:
         this.setBounds(
             (desktopSolution.width - width) / 2,
             (desktopSolution.height - height) / 2,
@@ -38,6 +45,7 @@ public class GameFrame extends JFrame {
         );
     }
 
+    //O método abaixo inicia o jogo, tornando a tela visível
     public void open() {
         stateManager.getScreen().startGame();
         this.setVisible(true);
--- src/com/gamestudio/interfaces/Screen.java
+++ src/com/gamestudio/interfaces/Screen.java
@@ -8,9 +8,10 @@ import javax.swing.JPanel;
 
 import com.gamestudio.state.State;
 
+// A classe abaixo é reponsável por organizar e agrupar os componentes gráficos
 public class Screen extends JPanel implements Runnable, KeyListener{
-    private State state;
-    Thread gameThread;
+    private State state; // Estado atual do jogo
+    Thread gameThread; // Thread utilizado no GameLoop
 
     public Screen(State state) {
         this.state = state;
@@ -20,31 +21,35 @@ public class Screen extends JPanel implements Runnable, KeyListener{
         this.state = state;
     }
 
+    // O método abaixo cria e inicia uma nova thread para rodar o jogo separadamente da thread principal da interface gráfica.
     public void startGame(){
         gameThread = new Thread(this);
         gameThread.start();
     }
 
+    // Método responsável por exibir todos os componentes do estado atual na tela:
     @Override
     public void paint(Graphics g) {
         g.drawImage(state.getBufferedImage(), 0, 0, null);
     }
 
+    // Método chamado caso alguma tecla seja pressionada:
     @Override
     public void keyPressed(KeyEvent e) {
         this.state.setPressedButton(e.getKeyCode());
     }
 
+     // Método chamado caso alguma tecla seja solta:
     @Override
     public void keyReleased(KeyEvent e) {
         this.state.setReleasedButton(e.getKeyCode());
     }
 
     @Override
-    public void keyTyped(KeyEvent e) {
+    public void keyTyped(KeyEvent e) {}
 
-    }
-
+    // O método abaixo implementa o Loop do Jogo
+    // Para realizar o loop o método inicia a execução do jogo em uma thread separada
     @Override
     public void run() {
 
@@ -56,9 +61,10 @@ public class Screen extends JPanel implements Runnable, KeyListener{
 
         while(true){
 
-            state.update();
-            state.render();
-            repaint();
+            
+            state.update(); // Atualiza o estado atual do jogo
+            state.render(); // Renderiza todos os elementos presentes no estado
+            repaint(); // Exibi os elementos renderizados na tela
 
             currentTime = System.nanoTime();
             sleepTime = period - (currentTime - previousTime);
